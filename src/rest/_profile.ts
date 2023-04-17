b4da8e7ca3ee0e0caaee8517188b5a861ca1a0bf
JoachimDauchot <dauchot.joachim@gmail.com>
2023-04-17 21:39:33 +0200

    profiel info teruggeven

---
+++ src/rest/_profile.ts
@@ -0,0 +1,44 @@
+import Router from "koa-router";
+import bedrijfService from "../service/bedrijf";
+import Koa, { Next } from "koa";
+import { logger } from "../server";
+
+
+
+
+
+// GET bedrijf by 'bedrijfId'
+export const getProfiel = async (ctx: Koa.Context) => {
+    ctx.body = await bedrijfService.getBedrijfProfiel(ctx);
+  };
+
+
+
+
+
+
+
+
+
+
+
+export default function installProfielRoutes(app: any) {
+    const router = new Router({
+      prefix: "/profiel",
+    });
+
+    /**
+     * PROTECTED ROUTES
+     */
+
+    router.get(
+        "/:id",
+        // authService.requireAuthentication,
+        getProfiel
+      );
+
+
+  
+    app.use(router.routes()).use(router.allowedMethods());
+    logger.debug(`Installation of profiel Route (_profiel.ts) completed`);
+  }
