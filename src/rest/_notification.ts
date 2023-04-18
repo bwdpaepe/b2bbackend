2a49f93ecc946ce2fdd0f75da4e6001f27274c29
DIMITRI VALCKENIER <dimitri.valckenier@student.hogent.be>
2023-04-18 11:25:19 +0200

    ophalen notificationList van user

--- src/rest/_notification.ts
+++ src/rest/_notification.ts
@@ -2,6 +2,8 @@ import Router from "koa-router";
 import notificationService from "../service/notification";
 import Koa, { Next } from "koa";
 import { logger } from "../server";
+import { Functions } from "../enums/Functions";
+import authService from "../service/auth";
 
 // GET rest route
 const checkNotificationEndpoint = async (ctx: Koa.Context) => {
@@ -11,6 +13,11 @@ const checkNotificationEndpoint = async (ctx: Koa.Context) => {
 // GET all notifications
 const getAllNotifications = async (ctx: Koa.Context) => {
   ctx.body = await notificationService.getAllNotifications();
+};
+
+// GET all notifications for a specific user
+const getAllNotificationsForUser = async (ctx: Koa.Context) => {
+  ctx.body = await notificationService.getAllNotificationsForUser(ctx);
 };
 
 export default function installNotificationRoutes(app: any) {
@@ -27,6 +34,18 @@ export default function installNotificationRoutes(app: any) {
   // GET all notifications
   router.get("/all", getAllNotifications);
 
+  /**
+   * PROTECTED ROUTES
+   */
+  // GET all notifications for a specific user
+  router.get(
+    "/user",
+    authService.requireAuthentication,
+    getAllNotificationsForUser
+  );
+
   app.use(router.routes()).use(router.allowedMethods());
-  logger.debug(`Installation of Notification Routes (_notification.ts) completed`);
+  logger.debug(
+    `Installation of Notification Routes (_notification.ts) completed`
+  );
 }
--- src/service/notification.ts
+++ src/service/notification.ts
@@ -2,6 +2,19 @@ import Koa from "koa";
 import { logger } from "../server";
 import { AppDataSource } from "../data-source";
 import { Notification } from "../entity/Notification";
+import authService from "./auth";
+import { User } from "../entity/User";
+import { Functions } from "../enums/Functions";
+import { BestellingStatus } from "../enums/BestellingStatusEnum";
+
+interface NotificationWithBestelling {
+  notificationID: number;
+  creationDate: Date;
+  isRead: boolean;
+  bestellingId: number;
+  orderId: string;
+  bestellingStatus: BestellingStatus;
+}
 
 const debugLog = (message: any, meta = {}) => {
   logger.debug(message);
@@ -9,6 +22,7 @@ const debugLog = (message: any, meta = {}) => {
 
 // Ophalen repository
 const notificationRepository = AppDataSource.getRepository(Notification);
+const usersRepository = AppDataSource.getRepository(User);
 
 /**
  * Check if the endpoint /api/notification/ is available.
@@ -26,11 +40,11 @@ const getAllNotifications = async () => {
   debugLog("GET list of all notifications");
   const notifications = await notificationRepository.find({
     relations: [
-      "aankoper", 
+      "aankoper",
       "aankoper.bedrijf",
-      "bestelling", 
-      "bestelling.leverancierBedrijf", 
-      "bestelling.klantBedrijf"
+      "bestelling",
+      "bestelling.leverancierBedrijf",
+      "bestelling.klantBedrijf",
     ],
   });
 
@@ -45,8 +59,62 @@ const getAllNotifications = async () => {
   return response;
 };
 
+/**
+ * GET list of all notifications for a specific user.
+ */
+const getAllNotificationsForUser = async (ctx: Koa.Context) => {
+  const JWTUserInfo = await authService.checkAndParseSession(
+    ctx.headers.authorization
+  );
+  const JWTuserId = JWTUserInfo.userId;
+  debugLog(
+    "GET list of all notifications for a specific user with id: " + JWTuserId
+  );
+  const user = await usersRepository.findOne({
+    where: { userId: JWTuserId },
+  });
+
+  if (!user) {
+    debugLog("User with userId " + JWTuserId + " doesn't exist");
+    return (ctx.status = 404), (ctx.body = "User doesn't exist" + JWTuserId);
+  }
+  debugLog(
+    "User with userId " +
+      JWTuserId +
+      " exists, name: " +
+      user.firstname +
+      " " +
+      user.lastname
+  );
+
+  if (user.function.toLowerCase() !== Functions.AANKOPER.toLowerCase()) {
+    debugLog(
+      "User with userId " + JWTuserId + " is not an " + Functions.AANKOPER
+    );
+    return (
+      (ctx.status = 404), (ctx.body = "User is not an " + Functions.AANKOPER)
+    );
+  }
+  debugLog("User with userId " + JWTuserId + " is an " + Functions.AANKOPER);
+
+  const notifications = await notificationRepository
+    .createQueryBuilder("n")
+    .select([
+      "n.ID as notificationID",
+      "n.ISBEKENEN as isRead",
+      "b.ID as bestellingId",
+      "b.ORDERID as orderId",
+      "b.STATUS as bestellingStatus",
+    ])
+    .innerJoin("n.bestelling", "b")
+    .where("b.Medewerker = :userId", { userId: JWTuserId })
+    .getRawMany();
+
+  return notifications;
+};
 
 export default {
   checkNotificationEndpoint,
   getAllNotifications,
+  getAllNotificationsForUser,
 };
