c3a464f9fbe06c511f3e146a9a2b4f0fb8ecadfc
Ian Daelman <ian.daelman@student.hogent.be>
2023-04-28 09:51:31 +0200

    Get product by id implementatie

--- src/rest/_product.ts
+++ src/rest/_product.ts
@@ -21,6 +21,14 @@ export default function installProductRoutes(app: any) {
     ctx.body = await productService.getAllProductsByBedrijfId(ctx);
   };
 
+  const getProductById = async (ctx: Koa.Context) => {
+    ctx.body = await productService.getProductById(ctx);
+  };
+
+  const getAllProductByBedrijfIdAndProductId = async (ctx: Koa.Context) => {
+    ctx.body = await productService.getAllProductByBedrijfIdAndProductId(ctx);
+  };
+
   /**
    * PUBLIC ROUTES
    */
@@ -31,6 +39,14 @@ export default function installProductRoutes(app: any) {
   // GET all products by 'bedrijfId'
   // example http://localhost:9000/api/products/bedrijfId?bedrijfId=2
   router.get("/bedrijfId", getAllProductsByBedrijfId);
+
+  //GET product by 'productId'
+  // example http://localhost:9000/api/products/productId?productId=2
+  router.get("/productId", getProductById);
+
+  //GET product by 'productId' and 'bedrijfId'
+  // example http://localhost:9000/api/products/productIdBedrijfId?productId=2&bedrijfId=2
+  router.get("/productIdBedrijfId", getAllProductByBedrijfIdAndProductId);
 
   app.use(router.routes()).use(router.allowedMethods());
   logger.debug(`Installation of Product Routes (_product.ts) completed`);
