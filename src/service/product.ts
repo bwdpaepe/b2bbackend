b13c570160e792fb342a2a2cb40dffd055f3d5e4
Ian Daelman <ian.daelman@student.hogent.be>
2023-04-28 09:51:31 +0200

    Get product by id implementatie

--- src/service/product.ts
+++ src/service/product.ts
@@ -3,7 +3,7 @@ import { logger } from "../server";
 import { AppDataSource } from "../data-source";
 import { Product } from "../entity/Product";
 
-const debugLog = (message: any, meta = {}) => {
+const debugLog = (message: any) => {
   logger.debug(message);
 };
 
@@ -45,7 +45,71 @@ const getAllProductsByBedrijfId = async (ctx: Koa.Context) => {
   }
 };
 
+const getProductById = async (ctx: Koa.Context) => {
+  try {
+    debugLog("GET product with productId " + ctx.query.productId);
+    const productId = Number(ctx.query.productId);
+
+    if (!productId) {
+      throw new Error("No correct productId was provided");
+    }
+
+    const product = await productRepository.findOne({
+      where: { productId: productId },
+    });
+
+    if (!product) {
+      return (
+        (ctx.status = 204),
+        (ctx.body = { error: "No product found for productId " + productId })
+      );
+    }
+
+    return product;
+  } catch (error: any) {
+    return (ctx.status = 400), (ctx.body = { error: error.message });
+  }
+};
+
+const getAllProductByBedrijfIdAndProductId = async (ctx: Koa.Context) => {
+  try {
+    debugLog(
+      "GET product with productId " +
+        ctx.query.productId +
+        " with bedrijfId " +
+        ctx.query.bedrijfId
+    );
+    const bedrijfId = Number(ctx.query.bedrijfId);
+    const productId = Number(ctx.query.productId);
+
+    if (!bedrijfId) {
+      throw new Error("No correct bedrijfId was provided");
+    }
+
+    if (!productId) {
+      throw new Error("No correct productId was provided");
+    }
+
+    const product = await productRepository.findOne({
+      where: { bedrijf: { bedrijfId: bedrijfId }, productId: productId },
+    });
+
+    if (!product) {
+      return (
+        (ctx.status = 204),
+        (ctx.body = { error: "No product found for bedrijfId " + bedrijfId })
+      );
+    }
+
+    return product;
+  } catch (error: any) {
+    return (ctx.status = 400), (ctx.body = { error: error.message });
+  }
+};
+
 export default {
   checkProductEndpoint,
   getAllProductsByBedrijfId,
+  getProductById,
+  getAllProductByBedrijfIdAndProductId,
 };
